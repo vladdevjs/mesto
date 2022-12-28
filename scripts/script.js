5ad45dbf568b462fa6a2bc9fb4ed490ee84d4723
Vlad Umarov <vladdevs@yandex.ru>
2022-12-28 18:48:40 +0300

    feat: реализовать функциональность редактирования данных профиля

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+let buttonEdit = document.querySelector(".profile__edit");
+let buttonClose = document.querySelector(".popup__close");
+let popup = document.querySelector(".popup");
+let formElement = document.querySelector(".form");
+let nameInput = document.querySelector(".form__field_type_name");
+let jobInput = document.querySelector(".form__field_type_job");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__description");
+
+function showPopup() {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+}
+
+buttonEdit.addEventListener("click", showPopup);
+buttonClose.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleFormSubmit);
